3a75c3fedd945a0339b7ba4026b4dda059f5b271
Amador Custer <custeramador@gmail.com>
2021-08-17 15:50:57 -0500

    set placeholder attribute as optional

--- lib/panda_doc/objects/field.rb
+++ lib/panda_doc/objects/field.rb
@@ -6,7 +6,7 @@ module PandaDoc
       attribute :uuid, Types::Coercible::String
       attribute :name, Types::Coercible::String.optional
       attribute :title, Types::Coercible::String.optional
-      attribute :placeholder, Types::Coercible::String.optional
+      attribute? :placeholder, Types::Coercible::String.optional
       attribute :value, Types::Nil | Types::Hash | Types::Coercible::String
       attribute? :assigned_to, PandaDoc::Objects::Recipient
     end
