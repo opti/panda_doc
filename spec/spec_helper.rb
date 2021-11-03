e0c46c5b96160cb5b72254ad2a35e9e1f09dc461
Igor Pstyga <igor.pstyga@gmail.com>
2021-11-03 11:15:22 -0700

    Switch to github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "simplecov"
-SimpleCov.start
+if ENV["SIMPLECOV"] == "1"
+  require "simplecov"
+  SimpleCov.start
+end
 
 require "logger"
 require "dry/configurable/test_interface"
