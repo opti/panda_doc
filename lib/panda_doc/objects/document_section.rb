77bd38c136bf0e879c57c18406ea2d562266f1bb
John Lannon <john@aboon.com>
2025-07-22 11:27:53 -0500

    fix: make `DocumentSection` attributes optional
    Only the `uuid` and `name` attributes are present on results from the list operation.
    This marks the other attributes as optional.

--- lib/panda_doc/objects/document_section.rb
+++ lib/panda_doc/objects/document_section.rb
@@ -2,12 +2,12 @@ module PandaDoc
   module Objects
     class DocumentSection < Base
       attribute :uuid, Types::String
-      attribute :document_uuid, Types::String
-      attribute :status, Types::Custom::DocumentStatus
+      attribute? :document_uuid, Types::String
+      attribute? :status, Types::Custom::DocumentStatus
       attribute :name, Types::String
 
-      attribute :date_created, Types::Params::DateTime
-      attribute :date_modified, Types::Params::DateTime
+      attribute? :date_created, Types::Params::DateTime
+      attribute? :date_modified, Types::Params::DateTime
 
       alias_method :created_at, :date_created
       alias_method :updated_at, :date_modified
